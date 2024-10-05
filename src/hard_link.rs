19c1ca7b6fd53d5d620f18582f923825fc709174
gilsaia <794433219@qq.com>
2024-10-05 13:54:41 +0800

    [feat] hard link check pattern

--- src/hard_link.rs
+++ src/hard_link.rs
@@ -18,21 +18,31 @@ pub struct HardLinkArgs {
         help = "Copy File Size Threshold,default is 100 MB."
     )]
     threshold: u64,
+    #[arg(long, help = "Pattern used to filter file")]
+    pattern: Option<String>,
 }
 
-fn filter_file(entry: &DirEntry, copy_all: bool, threshold: u64) -> bool {
-    if copy_all {
-        return true;
-    }
+fn filter_file(entry: &DirEntry, copy_all: bool, threshold: u64, pattern: &Option<String>) -> bool {
     let path = entry.path();
     let ext = path.extension().unwrap();
-    if !(ext == "mp4" || ext == "mkv" || ext == "avi" || ext == "mov" || ext == "wmv") {
+    if !copy_all && !(ext == "mp4" || ext == "mkv" || ext == "avi" || ext == "mov" || ext == "wmv")
+    {
         return false;
     }
     let meta = entry.metadata().unwrap();
     if meta.len() <= (threshold * 1024 * 1024) {
         return false;
     }
+    match pattern {
+        Some(pt) => {
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+            if !file_name.contains(pt) {
+                return false;
+            }
+        }
+        None => {}
+    }
+
     return true;
 }
 
@@ -47,7 +57,7 @@ pub fn hard_link_files(args: &HardLinkArgs) -> Result<(), Box<dyn std::error::Er
     }
     for entry in fs::read_dir(base_path)? {
         let entry = entry?;
-        if filter_file(&entry, args.copy_all, args.threshold) {
+        if filter_file(&entry, args.copy_all, args.threshold, &args.pattern) {
             hard_link(entry.path(), dst_path.join(entry.file_name()))?;
         }
     }
